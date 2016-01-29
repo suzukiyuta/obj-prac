7be99d2f893196baa99202a9c16c4aa790e8029b
yuuta <poke.d.p.0931-y.s@docomo.ne.jp>
2016-01-29 16:08:28 +0900

    0129

---
+++ obj-prac/CShikaku.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace obj_prac
+{
+    class CShikaku: CZukei
+    {
+        void instantiateShikaku()
+        {
+            // 種類を四角形にする
+            type.Add(TYPE.SHIKAKU);
+            // 底辺に1～10の乱数を求める
+            teihen.Add(rand.Next(1, 11));
+            // 高さに1～99の乱数を求める
+            takasa.Add(rand.Next(1, 11));
+        }
+    }
+}
---
+++ obj-prac/CZukei.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace obj_prac
+{
+   abstract class CZukei
+    {
+      protected static Random rand = new Random();
+        /** 種類。処理を分けるのに利用する*/
+      protected  List<TYPE> type;
+        /** 底辺*/
+       protected List<float> teihen;
+        /** 高さ*/
+       protected List<float> takasa;
+
+    }
+
+    
+    public CZukei()
+    {
+         // 底辺に1～10の乱数を求める
+            teihen.Add(rand.Next(1, 11));
+            // 高さに1～10の乱数を求める
+            takasa.Add(rand.Next(1, 11));
+    }
+
+}
+
